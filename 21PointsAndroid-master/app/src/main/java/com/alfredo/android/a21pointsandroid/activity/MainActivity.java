db03b4fc5f9f31bdc3565bd115d9bb1da959a3ab
MiquelZ <miquel.zamora@students.salle.url.edu>
2019-07-19 17:44:44 +0200

    SplashScreen advance

--- 21PointsAndroid-master/app/src/main/java/com/alfredo/android/a21pointsandroid/activity/MainActivity.java
+++ 21PointsAndroid-master/app/src/main/java/com/alfredo/android/a21pointsandroid/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.alfredo.android.a21pointsandroid.activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +13,22 @@ public class MainActivity extends AppCompatActivity {
 
     private Button registerBtn;
     private Button signInBtn;
+    private static int SPASH_TIME_OUT = 10000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        }, SPASH_TIME_OUT);*/
 
         registerBtn = (Button) findViewById(R.id.menu_register);
         signInBtn = (Button) findViewById(R.id.menu_sign_in);
